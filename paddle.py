038eca29498a4907177969d2f6b1e6ff306da7e5
Jacob <Jacob@DESKTOP-77NR0O9>
2024-11-01 07:22:36 +0200

    add paddle.py

---
+++ paddle.py
@@ -0,0 +1,21 @@
+from brick import Brick
+
+
+class Paddle(Brick):
+    DEFAULT_WIDTH = 150
+    DEFAULT_HEIGHT = 25
+    DEFAULT_MOVE_SPEED = 2
+
+    def __init__(self, screen):
+        x = (screen.width - self.DEFAULT_WIDTH) // 2
+        y = screen.height - 50
+        width = self.DEFAULT_WIDTH
+        height = self.DEFAULT_HEIGHT
+        super().__init__(x, y, width, height, screen, "white")
+        self.move_speed = self.DEFAULT_MOVE_SPEED
+        self.x_vel = 0
+
+    def update(self, delta_time):
+        self.x += self.x_vel * delta_time * self.move_speed
+        self.x = min(self.x, self.screen.width - self.width)
+        self.x = max(self.x, self.height * -1)
